b7f40784341a1031264362abdf1a702c40975137
gededhani <dhanipradipta777@gmail.com>
2023-08-05 21:07:04 +0800

    scrapy_artikel

--- artikel/artikel/spiders/inbizia.py
+++ artikel/artikel/spiders/inbizia.py
@@ -4,15 +4,20 @@ import scrapy
 class InbiziaSpider(scrapy.Spider):
     name = "inbizia"
     allowed_domains = ["www.inbizia.com"]
-    start_urls = ["https://www.inbizia.com/forex/berita"]
+    start_urls = ["https://www.inbizia.com/forex/berita?s=1"]
 
     def parse(self, response):
+        urls = 'https://www.inbizia.com'
         for news in response.css('div.col-8.lh-120'):
-            urls = 'https://www.inbizia.com'
             yield {
                 'Title':news.css('a.font-link::text').get(),
                 'Link':urls+news.css('a.font-link::attr(href)').get()
             }
+
+        next_page = urls + response.css('a.page-link::attr(href)')[5].get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
+
 
 
 
